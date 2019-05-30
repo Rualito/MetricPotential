040c59c6d89febe8c3e094bdbc59f1c995f2e8f9
raul <raulfilipe1998@hotmail.com>
2019-05-30 20:14:29 +0100

    putting stuff in

---
+++ CProjects/MetricPotential/main.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include "cFCgraphics.h"
+#include "Tpad.h"
+#include "TGraph.h"
+#include "Integrator.h"
+
+
+double black_hole_metric_distance(double r1, double r2, double th1, double th2){
+
+    return 0;
+}
+
+
+int main(){
+    
+    
+    
+}
